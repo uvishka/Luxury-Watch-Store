c57e6513cac873e096f8888ccfb653003cc88ad4
uvishka <uvishka@gmail.com>
2024-06-27 23:17:35 +0530

    up

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
         };
 
         axios
-            .post("http://watch:5555/users/login", data)
+            .post("http://backend:5555/users/login", data)
             .then((response) => {
                 if (response.status === 200) {
                     console.log("Login successful");
